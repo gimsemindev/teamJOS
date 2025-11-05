0c239bedb15b26fb92b1021ad103bf9402f97334
user <user@DESKTOP-LFUIKR8>
2025-11-05 12:01:01 +0900

    제발올라가라

--- teamJOS/src/com/sp/Test1.java
+++ teamJOS/src/com/sp/Test1.java
@@ -2,6 +2,6 @@ package com.sp;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("hello World! by Seok Jun");
+		System.out.println("hello World22222! by Seok Jun");
 	}
 }
