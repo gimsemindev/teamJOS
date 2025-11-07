853e4963baf1d1865f5b066b9da5797801b17952
user <user@DESKTOP-LFUIKR8>
2025-11-07 17:50:07 +0900

    des

--- teamJOS/src/com/sp/dao/impl/BoardDAOImpl.java
+++ teamJOS/src/com/sp/dao/impl/BoardDAOImpl.java
@@ -1,17 +1,37 @@
 package com.sp.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.sp.dao.BoardDAO;
 import com.sp.model.BoardDTO;
+import com.sp.util.DBConn;
 
 public class BoardDAOImpl implements BoardDAO{
 
 	@Override
 	public int insertPost(BoardDTO board) throws SQLException{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		/*
+		Connection conn = DBConn.getConnection();
+		BoardDTO 
+		PreparedStatement pstmt = null;
+		String sql;
+		
+		try {
+			
+			sql ="INSERT INTO tb_board(BOARD_SEQ, EMP_NO  ,TITLE ,CONTENT , REG_DTM ) VALUES (?,?, ?, ?,SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.get());
+			pstmt.setString(2, dto.getName());
+			pstmt.setString(3, dto.getPwd());
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		
+	*/return 0;}
 
 	@Override
 	public int updatePost(BoardDTO board) throws SQLException{
